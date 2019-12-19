61f3edb42e3cf1454798c1cb4dba05118df0a685
Hector Valdez <hvaldez180@gmail.com>
2019-12-18 21:20:59 -0500

    added delete functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -41,7 +41,6 @@ export default class Movie extends React.Component {
     axios
       .delete(`http://localhost:5000/api/movies/${this.state.movie.id}`)
       .then(res => {
-        this.props.updateItems(res.data);
         this.props.history.push('/');
       })
       .catch(err => console.log(err));
